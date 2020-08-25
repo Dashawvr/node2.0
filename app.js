95f3a759f81d4c3c1a0c8d2acad66dada5db7c59
Dashawvr <dasha.onishchenko@gmail.com>
2020-08-25 17:48:24 +0300

    Login, Registration

--- app.js
+++ app.js
@@ -5,22 +5,32 @@ const path = require('path');
 
 app.use(express.urlencoded({extended: true}))
 app.use(express.json())
-
 app.use(express.static(path.join(process.cwd(), 'views')))
 
+// settings
 app.set('view engine', '.hbs')
 app.engine('.hbs', expressHandlebars({defaultLayout: false}))
 app.set('views', path.join(process.cwd(), 'views'))
 
+//registration
 app.get('/render-reg', (req, res)=> {
     res.render('registration');
 })
 
 app.post('/reg', (req,res)=> {
     console.log(req.body);
-    res.end('Register successfully')
+    // res.end('Register successfully')
+    res.redirect('/');
 })
-
+//logination
+app.get('/login', (req, res)=> {
+    res.render('login');
+})
+app.post('/login', (req, res)=> {
+    console.log(req.body);
+    // res.end('Login successfully')
+    res.redirect('/');
+})
 
 ////
 app.get('/', (req, res)=> {
@@ -38,11 +48,11 @@ app.get('/users', (req, res)=> {
     ])
 });
 
-
 app.post('/users', (req, res)=> {
     res.end('All users here')
 });
 
+//port
 app.listen(5000, (err) => {
     if (err) {
         console.log(err);
